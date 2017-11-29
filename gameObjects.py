03dec8c2961f8c2b5e3e71d63787dd8c1b1ea09e
Suhas <suhas.2ab@gmail.com>
2017-11-29 18:08:03 +0530

    powerups
    introducing powerups!

--- gameObjects.py
+++ gameObjects.py
@@ -2,34 +2,48 @@ import pygame
 from pygame.locals import *
 
 class Paddle():
-	def __init__(self, x, y, width, height, velocity):
-		self.x= x
-		self.y= y
-		self.width= width
-		self.height= height
-		self.velocity= velocity
+    def __init__(self, x, y, width, height, velocity):
+        self.x= x
+        self.y= y
+        self.width= width
+        self.height= height
+        self.velocity= velocity
 
-	def getPaddle(self):
-		return pygame.Rect(self.x,self.y,self.width,self.height)
+    def getPaddle(self):
+        return pygame.Rect(self.x,self.y,self.width,self.height)
 
 class Puck():
-	def __init__(self, x, y, width, height, velocity):
-		self.x= x
-		self.y= y
-		self.init_x= x
-		self.init_y= y
-		self.width= width
-		self.height= height
-		self.velocity= velocity
-		self.serveDirection= 1
+    def __init__(self, x, y, width, height, velocity):
+        self.x= x
+        self.y= y
+        self.init_x= x
+        self.init_y= y
+        self.width= width
+        self.height= height
+        self.velocity= velocity
+        self.serveDirection= 1
 
-	def getPuck(self):
-		return pygame.Rect(self.x,self.y,self.width,self.height)
+    def getPuck(self):
+        return pygame.Rect(self.x,self.y,self.width,self.height)
 
-	def reset(self):
-	    self.velocity[0]=10*self.serveDirection
-	    self.velocity[1]=4*self.serveDirection
-	    self.x= self.init_x
-	    self.y= self.init_y
+    def reset(self):
+        self.velocity[0]=10*self.serveDirection
+        self.velocity[1]=4*self.serveDirection
+        self.x= self.init_x
+        self.y= self.init_y
+
+
+class powerup1():
+    def __init__(self,x,y,width,height):
+        self.active=True
+        self.x=x
+        self.y=y 
+        self.width=width
+        self.height=height
+
+    def getpowerup1(self):
+        return pygame.Rect(self.x,self.y,self.width,self.height)
+    def kill(self):
+        self.active=False
 
 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import pygame, sys
 from pygame.locals import *
 from gameObjects import *
-
+from random import randint
 pygame.init()
 clock= pygame.time.Clock()
 screen= pygame.display.set_mode((800,600))
-
+pygame.time.set_timer(USEREVENT + 1, 1000) # Used to correctly implement seconds
+  
 #Create Game Objects
-
+initht=80
 paddleVelocity= 10
 paddle1= Paddle(10, screen.get_height()/2 -40 , 10, 80, paddleVelocity)
 paddle2= Paddle(screen.get_width()-20, screen.get_height()/2 -40, 10, 80, paddleVelocity)
@@ -16,17 +17,32 @@ puckVelocity= [8,4]
 puck= Puck(screen.get_width()/2, screen.get_height()/2, 20, 20, puckVelocity)
 
 divider= pygame.Rect(screen.get_width()/2,0,3,screen.get_height())
+powerup1 = powerup1(screen.get_width()/2, screen.get_height()/2, 8, 25)
 
 
 #Score
 score1,score2=0,0
-
-
+collideflag=0
+seconds = 0
+randomflag=1
+time2 =0
+time1 =0
 #Game Loop
 while True:
+    # timer for making the powerups disappear
+
     for event in pygame.event.get():
         if event.type==QUIT:
             sys.exit()
+        elif event.type == USEREVENT + 1:
+                seconds+=1
+                time2+=1
+    #generating random x and y coordinates for the powerup
+    if powerup1.active == True and randomflag == 1:
+        powerup1.x=randint(0,screen.get_width())
+        powerup1.y=randint(0,screen.get_height())
+        randomflag=0
+        
 
     w,s,up,down,d,a,right,left=0,0,0,0,0,0,0,0
     #Process Player Input
@@ -88,18 +104,51 @@ while True:
         puck.velocity[1]*=-1
     if puck.getPuck().colliderect(paddle1.getPaddle()) or puck.getPuck().colliderect(paddle2.getPaddle()):
         puck.velocity[0]*=-1
+    if powerup1.getpowerup1().colliderect(paddle1.getPaddle()) and powerup1.active==True :
+        paddle1.height*=2
+        collideflag=1
+        
+    if powerup1.getpowerup1().colliderect(paddle2.getPaddle()) and powerup1.active==True :
+        paddle2.height*=2
+        collideflag=1
+    
+    if collideflag==1:
+        powerup1.kill() #powerup1.active = False
+        time1 = seconds
+        collideflag = 0
+        randomflag = 1 
+    
+   # print("time1="+str(time1))
+   # print("seconds="+str(seconds))
+    #making powerup pop up every 10 seconds 
+    if powerup1.active == False and seconds > time1+15:
+        time2=0 
+        powerup1.active = True
+    #reducing the length of paddles after taking the powerup after 10seconds
+    if paddle1.height>80 and seconds > time1 +15:
+            paddle1.height/=2
+    if paddle2.height>80 and seconds>time1+15:   #reverting it after  15 seconds
+            paddle2.height/=2
+    if time2>5 :
+        
+        powerup1.active = False
+        randomflag =1
 
 
     #Render Logic
     screen.fill((0,40,40))
 
-    pygame.draw.rect(screen, (255,0, 0), paddle1.getPaddle())
-    pygame.draw.rect(screen, (255,255,0), paddle2.getPaddle())        
-    pygame.draw.circle(screen, (255,255,255), (puck.x, puck.y), int(puck.width/2))
+    pygame.draw.rect(screen, (255,67, 222), paddle1.getPaddle())
+    pygame.draw.rect(screen, (255,255,125), paddle2.getPaddle())        
+    pygame.draw.circle(screen, (255,255,255), (int(puck.x), int(puck.y)), int(puck.width/2))
     pygame.draw.rect(screen, (255,255,255), divider)
-
+    if powerup1.active==True:
+        time1=seconds        
+        pygame.draw.rect(screen,(255,255,0),powerup1.getpowerup1(),2)
+        
     pygame.display.flip()
     clock.tick(60)
+  
 
 
 
