3dbcc095100e3512842d423be74040239d54f8f7
thealphadollar <shivam.cs.iit.kgp@gmail.com>
2017-12-03 00:30:46 +0530

    all corrected

--- gameObjects.py
+++ gameObjects.py
@@ -1,21 +1,4 @@
 import pygame
-<<<<<<< HEAD
-
-class Paddle():
-    def __init__(self, x, y, width, height, velocity):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.velocity = velocity
-
-    def getPaddle(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
-
-
-class Puck():
-    def __init__(self, x, y, width, height, velocity):
-=======
 import math
 
 
@@ -57,26 +40,10 @@ class Paddle():
 
 class Puck():
     def __init__(self, x, y, radius, velocity):
->>>>>>> 3386ee18a2b09b28bc8b3f2fa94ab284ab5c127a
         self.x = x
         self.y = y
         self.init_x = x
         self.init_y = y
-<<<<<<< HEAD
-        self.width = width
-        self.height = height
-        self.velocity = velocity
-        self.serveDirection = 1
-
-    def getPuck(self):
-        return pygame.Rect(self.x, self.y, self.width, self.height)
-
-    def reset(self):
-        self.velocity[0] = 10 * self.serveDirection
-        self.velocity[1] = 4 * self.serveDirection
-        self.x = self.init_x
-        self.y = self.init_y
-=======
         self.radius = radius
         self.velocity = velocity
         self.serveDirection = 1
@@ -88,7 +55,6 @@ class Puck():
         """
         Checks collision between circles using the distance formula:
         dist = sqrt((x2 - x1)**2 + (y2 - y1)**2)
-
         returns true if the distance is less than or equal to sum of
         radius of the puck and the paddle
         """
@@ -108,4 +74,3 @@ class Puck():
 
     def draw(self, screen):
         pygame.draw.circle(screen, (255, 255, 255), (self.x, self.y), self.radius)
->>>>>>> 3386ee18a2b09b28bc8b3f2fa94ab284ab5c127a
