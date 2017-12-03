f7984acefa903690e38a4ef5682d587f522445e5
Nityananda Gohain <nityanandagohain@gmail.com>
2017-12-03 13:32:28 +0530

    Start screen with interactive buttons
    Fixes #5

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from pygame.locals import *
 from gameObjects import *
+from startScreen import airHockeyStart
 
 # setting logo should take place before setting the display on some OS
 gamelogo = pygame.image.load('img/logo.png')
@@ -63,65 +64,78 @@ def renderPlayingArea():
 
 
 # Game Loop
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
+def gameLoop():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+        
+        global score1, score2
+        w, s, up, down, d, a, right, left = 0, 0, 0, 0, 0, 0, 0, 0
+        # Process Player 1 Input
+        w = pygame.key.get_pressed()[pygame.K_w]
+        s = pygame.key.get_pressed()[pygame.K_s]
+        d = pygame.key.get_pressed()[pygame.K_d]
+        a = pygame.key.get_pressed()[pygame.K_a]
+
+        # Process Player 2 Input
+        up = pygame.key.get_pressed()[pygame.K_UP]
+        down = pygame.key.get_pressed()[pygame.K_DOWN]
+        right = pygame.key.get_pressed()[pygame.K_RIGHT]
+        left = pygame.key.get_pressed()[pygame.K_LEFT]
+
+        # Update Logic
+
+        # Update Paddle1
+        paddle1.y += (s - w) * paddleVelocity
+        paddle1.x += (d - a) * paddleVelocity
+        paddle1.checkTopBottomBounds(height)
+        paddle1.checkLeftBoundary(width)
+
+        # Update Paddle2
+        paddle2.y += (down - up) * paddleVelocity
+        paddle2.x += (right - left) * paddleVelocity
+        paddle2.checkTopBottomBounds(height)
+        paddle2.checkRightBoundary(width)
+
+        # Update Puck
+        puck.x += puck.velocity[0]
+        puck.y += puck.velocity[1]
+        if puck.x + puck.radius < 0:
+            score2 += 1
+            puck.serveDirection = -1
+            puck.reset()
+        elif puck.x - puck.radius > width:
+            score1 += 1
+            puck.serveDirection = 1
+            puck.reset()
+        if puck.collidesTopBottom(height):
+            puck.velocity[1] *= -1
+        if puck.collidesWithPaddle(paddle1):
+            puck.x = paddle1.x + paddle1.radius + puck.radius
+            puck.velocity[0] *= -1
+        if puck.collidesWithPaddle(paddle2):
+            puck.x = paddle2.x - paddle2.radius - puck.radius
+            puck.velocity[0] *= -1
+
+        # playing area should be drawn first
+        renderPlayingArea()
+
+        # drawing the paddle and the puck
+        paddle1.draw(screen, (255, 0, 0))
+        paddle2.draw(screen, (255, 255, 0))
+        puck.draw(screen)
+
+        pygame.display.flip()
+        clock.tick(60)
+
+
+if __name__ == "__main__":
+        choice = airHockeyStart(screen, clock, width, height)
+        if choice == 1:
+            gameLoop()
+        elif choice == 0:
             sys.exit()
 
-    w, s, up, down, d, a, right, left = 0, 0, 0, 0, 0, 0, 0, 0
-    # Process Player 1 Input
-    w = pygame.key.get_pressed()[pygame.K_w]
-    s = pygame.key.get_pressed()[pygame.K_s]
-    d = pygame.key.get_pressed()[pygame.K_d]
-    a = pygame.key.get_pressed()[pygame.K_a]
 
-    # Process Player 2 Input
-    up = pygame.key.get_pressed()[pygame.K_UP]
-    down = pygame.key.get_pressed()[pygame.K_DOWN]
-    right = pygame.key.get_pressed()[pygame.K_RIGHT]
-    left = pygame.key.get_pressed()[pygame.K_LEFT]
 
-    # Update Logic
-
-    # Update Paddle1
-    paddle1.y += (s - w) * paddleVelocity
-    paddle1.x += (d - a) * paddleVelocity
-    paddle1.checkTopBottomBounds(height)
-    paddle1.checkLeftBoundary(width)
-
-    # Update Paddle2
-    paddle2.y += (down - up) * paddleVelocity
-    paddle2.x += (right - left) * paddleVelocity
-    paddle2.checkTopBottomBounds(height)
-    paddle2.checkRightBoundary(width)
-
-    # Update Puck
-    puck.x += puck.velocity[0]
-    puck.y += puck.velocity[1]
-    if puck.x + puck.radius < 0:
-        score2 += 1
-        puck.serveDirection = -1
-        puck.reset()
-    elif puck.x - puck.radius > width:
-        score1 += 1
-        puck.serveDirection = 1
-        puck.reset()
-    if puck.collidesTopBottom(height):
-        puck.velocity[1] *= -1
-    if puck.collidesWithPaddle(paddle1):
-        puck.x = paddle1.x + paddle1.radius + puck.radius
-        puck.velocity[0] *= -1
-    if puck.collidesWithPaddle(paddle2):
-        puck.x = paddle2.x - paddle2.radius - puck.radius
-        puck.velocity[0] *= -1
-
-    # playing area should be drawn first
-    renderPlayingArea()
-
-    # drawing the paddle and the puck
-    paddle1.draw(screen, (255, 0, 0))
-    paddle2.draw(screen, (255, 255, 0))
-    puck.draw(screen)
-
-    pygame.display.flip()
-    clock.tick(60)
