00fa6fd7770557acf52f8a5478640bdddbde674f
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2017-12-11 11:28:25 +0530

    Added global class
    Fixes #104

---
+++ globals.py
@@ -0,0 +1,17 @@
+import constants as const
+
+smallfont = None
+score1, score2 = 0, 0
+
+# Sound globals.
+paddleHit = None
+goal_whistle = None
+backgroundMusic = None
+
+# game globals.
+clock = None
+screen = None
+screenColor = (224, 214, 141)
+
+# width and height of the screen.
+width, height = const.WIDTH, const.HEIGHT
